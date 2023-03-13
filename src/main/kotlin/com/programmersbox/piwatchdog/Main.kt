0b94ee46f1c876693cb416edf48e37eb4dcedca7
Jacob Rein <jakerein@gmail.com>
2023-03-13 09:08:05 -0400

    Adding better update support! This will allow me to have an entire update script to run after downloading the new zip

--- src/main/kotlin/com/programmersbox/piwatchdog/Main.kt
+++ src/main/kotlin/com/programmersbox/piwatchdog/Main.kt
@@ -1,6 +1,7 @@
 package com.programmersbox.piwatchdog
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import io.ktor.client.*
@@ -17,14 +18,17 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import me.tongfei.progressbar.ProgressBarBuilder
-import java.io.BufferedReader
-import java.io.File
-import java.io.InputStreamReader
+import java.io.*
 import java.util.stream.Collectors
+import java.util.zip.ZipFile
 import kotlin.math.roundToInt
 import kotlin.time.Duration.Companion.days
+import kotlin.time.Duration.Companion.hours
+
 
 const val LOG_COLOR = 0x00ff00
+
+val desktopPath get() = System.getProperty("user.home") + "/Desktop"
 
 fun main(args: Array<String>) = runBlocking {
     runPythonCodeAsync(
@@ -32,6 +36,7 @@ fun main(args: Array<String>) = runBlocking {
         "Starting PillCounter",
         "Please Wait",
         "Taking Some Time",
+        "Sorry!",
         "Almost!"
     ).await()
     println("Program arguments: ${args.joinToString()}".color(LOG_COLOR))
@@ -65,6 +70,7 @@ fun checkForUpdate(oldVersion: String, newVersion: String): Boolean = try {
 class WatchDog : CliktCommand() {
     val jarPath by option("-j", help = "Path to Jar").required()
     val updateUrl by option("-u", help = "Update Url").required()
+    val folderName by option("-f", help = "Folder Name").default("Server")
 
     override fun run() = runBlocking {
         val versionFile = File("version.txt")
@@ -92,7 +98,6 @@ class WatchDog : CliktCommand() {
 
         val versionCheck = async {
             while (true) {
-                delay(1.days.inWholeMilliseconds)
                 try {
                     val response = client.get(updateUrl)
                         .bodyAsText()
@@ -114,13 +119,18 @@ class WatchDog : CliktCommand() {
                             .build()
                         val httpResponse: HttpResponse = client.get(response.jarUrl) {
                             onDownload { bytesSentTotal, contentLength ->
-                                //TODO: See if we can animate a little thing on the screen
                                 val progress = (bytesSentTotal * 100f / contentLength).roundToInt()
                                 pb.stepTo(progress.toLong())
                             }
                         }
                         pb.close()
-                        File(jarPath).writeBytes(httpResponse.bodyAsChannel().toByteArray())
+                        File("$desktopPath/$folderName.zip").writeBytes(httpResponse.bodyAsChannel().toByteArray())
+                        UnzipUtils.unzip(File("$desktopPath/$folderName.zip"), "$desktopPath/$folderName") {
+                            println("Unzipping $it%".color(LOG_COLOR))
+                        }
+                        runAsync("sh", "$desktopPath/$folderName/update.sh")
+                            .await()
+                            .also { println(it.color(LOG_COLOR)) }
                         delay(5000)
                         println("Downloaded".color(LOG_COLOR))
                         isServerRunning.emit(true)
@@ -128,6 +138,7 @@ class WatchDog : CliktCommand() {
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
+                delay(1.hours.inWholeMilliseconds)
             }
         }
 
@@ -146,7 +157,8 @@ class WatchDog : CliktCommand() {
             if (it) {
                 println("File exists? ${File(jarPath).exists()}".color(LOG_COLOR))
 
-                process = runtime.exec(arrayOf("java", "-jar", jarPath))
+                //process = runtime.exec(arrayOf("java", "-jar", jarPath))
+                process = runtime.exec(arrayOf("sudo", jarPath))
 
                 println("Pid: ${process!!.pid()}".color(LOG_COLOR))
                 process!!.inputStream.bufferedReader().use { r ->
@@ -194,6 +206,7 @@ object AnsiColor {
         val b = (this and 0xff)// / 255.0f
         return Triple(r, g, b)
     }
+
     private fun Long.valueOf(): Triple<Long, Long, Long> {
         val r = (this shr 16 and 0xff)// / 255.0f
         val g = (this shr 8 and 0xff)// / 255.0f
@@ -216,4 +229,78 @@ suspend fun runAsync(vararg command: String) = coroutineScope {
         val reader = BufferedReader(InputStreamReader(process.inputStream))
         reader.lines().collect(Collectors.joining("\n"))
     }
+}
+
+/**
+
+UnzipUtils class extracts files and sub-directories of a standard zip file to
+a destination directory.
+ */
+object UnzipUtils {
+    /*
+     * @param zipFilePath
+     * @param destDirectory
+     * @param updateProgress interface to update progress on UI
+     * @throws IOException
+    */
+    fun unzip(
+        zipFilePath: File,
+        destDirectory: String,
+        updateProgress: (progress: Int) -> Unit
+    ) {
+        File(destDirectory).run {
+            if (!exists()) {
+                mkdirs()
+            }
+        }
+        ZipFile(zipFilePath).use { zip ->
+            var entryCount = 0
+            val totalEntries = zip.entries().toList().size // files to unzip
+            zip.entries().asSequence().forEach { entry ->
+                zip.getInputStream(entry).use { input ->
+                    val filePath = destDirectory + File.separator + entry.name
+                    if (!entry.isDirectory) {
+                        // if the entry is a file, extracts it
+                        try {
+                            extractFile(input, filePath)
+                        } catch (e: Exception) {
+                            //e.printStackTrace()
+                        }
+                    } else {
+                        try {
+                            // if the entry is a directory, make the directory
+                            val dir = File(filePath)
+                            dir.mkdir()
+                        } catch (e: Exception) {
+                            //
+                        }
+                    }
+                    entryCount++
+                    updateProgress(((entryCount.toDouble() / totalEntries) * 100).toInt())
+                }
+            }
+        }
+    }
+
+    /**
+     * Extracts a zip entry (file entry)
+     * @param inputStream
+     * @param destFilePath
+     * @throws IOException
+     */
+    @Throws(IOException::class)
+    private fun extractFile(inputStream: InputStream, destFilePath: String) {
+        val bos = BufferedOutputStream(FileOutputStream(destFilePath))
+        val bytesIn = ByteArray(BUFFER_SIZE)
+        var read: Int
+        while (inputStream.read(bytesIn).also { read = it } != -1) {
+            bos.write(bytesIn, 0, read)
+        }
+        bos.close()
+    }
+
+    /**
+     * Size of the buffer to read/write data
+     */
+    private const val BUFFER_SIZE = 4096
 }
